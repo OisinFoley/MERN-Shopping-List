c3c76d25dc718f4d376f18128e7e36c9dcfb72a9
oisinfoley <oisinfoley@yahoo.co.uk>
2018-07-28 20:41:32 +0200

    Dependecies added, GET, POST and DELETE API endpoints in-place to handle shopping list items. Reactstrap added. Scripting to concurrently start server and client for in-place.

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI: 'mongodb://oisinfoley:p1nec0ne@ds127894.mlab.com:27894/oisinfoleymongo'
+  // mongoURI: 'mongodb://localhost:27017/'
+
+  // mongoose.connect('mongodb://localhost:27017/', function(err){
+
+}
